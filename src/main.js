958b5f7ba372463e84f9ae46f615f97f27923be5
alphonseb <alphonse.bouy@gmail.com>
2021-04-11 12:55:49 +0200

    switched from p5 package to cdn version

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
-import p5 from 'p5'
 import { timelineSketch } from './viz/timeline'
 import { graphSketch } from './viz/graph'
 
 new p5(timelineSketch);
-// new p5(graphSketch);
-
-// console.log(sketchInstance);
+new p5(graphSketch);
