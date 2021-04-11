cf9be22c62b528e1319b1596b6ccd2ed94c8d5c5
alphonseb <alphonse.bouy@gmail.com>
2021-04-11 22:01:23 +0200

    some slides

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { timelineSketch } from './viz/timeline'
 import { graphSketch } from './viz/graph'
-
 new p5(timelineSketch);
 new p5(graphSketch);
+
+// window.scrollBy(0, 300)
+window.addEventListener('wheel', ({ deltaX, deltaY }) => {
+    
+    if (!deltaX) {
+        console.log('hello', deltaY);
+        window.scrollBy(deltaY, 0)
+    }
+})
